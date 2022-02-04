26efb58762edb9cea34b522abf33f1a3ed022687
XanderGale <alessandro.galeazzi@icloud.com>
2022-02-04 01:24:47 +0100

    done php part, need restyling and header+footer

--- index.php
+++ index.php
@@ -43,19 +43,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google Faqs</title>
+    
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
-        
+
     </header>
 
     <main>
-        <?php foreach($faqs as $faq) { ?>
-            <h2> <?php echo $faq['question'] ?> </h2>
-            <?php foreach($faq['answers'] as $answer ) { ?>
-                <p> <?php echo $answer ?> </p>
+        <div class="container">
+            <?php foreach($faqs as $faq) { ?>
+                <h2> <?php echo $faq['question'] ?> </h2>
+                <?php foreach($faq['answers'] as $answer ) { ?>
+                    <p> <?php echo $answer ?> </p>
+                <?php } ?>
             <?php } ?>
-        <?php } ?>
+        </div>
     </main>
 
     <footer>
